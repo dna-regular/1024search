3e46952e795049f86804847845d91e5e7d25c9cb
rigensen <mr.right.0005@gmail.com>
2020-06-22 22:53:13 +0800

    modify

--- src/main.go
+++ src/main.go
@@ -1,37 +1,74 @@
 package main
 
 import (
-    "fmt"
-    "bytes"
-    "net/http"
-    "github.com/xfxdev/xlog"
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/xfxdev/xlog"
 )
 
 func LogInit() {
-    strLogLevel := "INFO"  // also maybe read from config.
-    logLevel, suc := xlog.ParseLevel(strLogLevel)
-    if suc == false {
-        // failed to parse log level, will use the default level[INFO] instead."
-    }
-    xlog.SetLevel(logLevel)
-    xlog.SetLayout("%L %f(%i)$ %l")
+	strLogLevel := "INFO" // also maybe read from config.
+	logLevel, suc := xlog.ParseLevel(strLogLevel)
+	if suc == false {
+		// failed to parse log level, will use the default level[INFO] instead."
+	}
+	xlog.SetLevel(logLevel)
+	xlog.SetLayout("%L %f(%i)$ %l")
+}
+
+func http_get(url string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		xlog.Error("create req err: ", err)
+		return ""
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		xlog.Error("get error: ", err)
+		return ""
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		xlog.Error("get error: ", url, resp.StatusCode)
+		return ""
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0, 1024*1024*2))
+	buf.ReadFrom(resp.Body)
+	return string(buf.Bytes())
+}
+
+func do_search(keyword string) []string {
+	urls := make([]string, 0, 100)
+	for i := 0; i < 100; i++ {
+		url := "http://t66y.com/thread0806.php?fid=7&search=&page=" + strconv.Itoa(i)
+		resp := http_get(url)
+		if strings.Contains(resp, keyword) {
+			fmt.Println(url)
+			urls = append(urls, url)
+		}
+		fmt.Printf("\r%d %s", i, url)
+	}
+	return urls
 }
 
 func main() {
-    xlog.Info("client start...")
-    resp, err := http.Get("http://t66y.com/thread0806.php?fid=7&search=&page=0")
-	if err != nil {
-        xlog.Error("get error")
+	LogInit()
+	xlog.Info("client start...")
+	if len(os.Args) != 2 {
+		print("usage:\n")
+		print("\t./search <keyward>\n")
 		return
 	}
-    headers := resp.Header
-    for k, v := range headers {
-		fmt.Printf("k=%v, v=%v\n", k, v)
+	urls := do_search(os.Args[1])
+	for url := range urls {
+		print(url)
 	}
-    defer resp.Body.Close()
-    buf := bytes.NewBuffer(make([]byte, 0, 512))
-	length, _ := buf.ReadFrom(resp.Body)
-	fmt.Println(len(buf.Bytes()))
-	fmt.Println(length)
-	fmt.Println(string(buf.Bytes()))
 }
